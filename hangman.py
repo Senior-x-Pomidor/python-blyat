18dfeec69c5f19581df303056a9858026fc08594
dani <kurtser.d@gmail.com>
2025-06-22 08:24:07 +0200

    Add hangman game functionality with difficulty selection and game modes

--- hangman.py
+++ hangman.py
@@ -1,5 +1,167 @@
 last_update = "22.06.2025"
 
+hangman_pictures =['''
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          ''','''
+     ┌      
+     │      
+     │      
+     │      
+     │      
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \ ''','''
+     ┌─────┐
+     │      
+     │      
+     │      
+     │      
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \ ''','''
+     ┌─────┐
+     │     │
+     │      
+     │      
+     │      
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │      
+     │      
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │     |
+     │     |
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │    /|
+     │     |
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │    /|\\
+     │     |
+     │      
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │    /|\\
+     │     |
+     │    / 
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │     O
+     │    /|\\
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │    (O
+     │    /|\\
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \ ''','''
+     ┌─────┐
+     │     │
+     │    (O)
+     │    /|\\
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │    (O)
+     │   _/|\\
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │    (O)
+     │   _/|\_
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │    (x)
+     │   _/|\_
+     │     |
+     │    / \\
+     │      
+     │      
+  ───┴───   
+ /       \  ''','''
+     ┌─────┐
+     │     │
+     │    (x)
+     │   _/|\_
+     │     |
+     │    / \\
+     │   GAME
+     │   OVER
+  ───┴───   
+ /       \  ''']
+
 def clear_terminal():
 
     # \033[H setzt den Cursor oben links, \033[J löscht bis zum Ende
@@ -10,7 +172,7 @@ def farbig(text, farbcode):
 
 def big_text(text):
 
-    x = 5
+    x = -5
 
     if text == "hangman":
         print("\033[1;31m    __  __                                            \033[0m"+" "*(10-x) + "\033[1;37m      ,%%%%%%%%,\033[0m")
@@ -26,15 +188,52 @@ def big_text(text):
         print(" "*(63-x)+"\033[1;37m          | | >>\033[0m")
         print(" "*(63-x)+"\033[1;37m          | |\033[0m")
         print(" "*(63-x)+"\033[1;37m         /   \\\033[0m")
-        print(" "*(63-x)+"\033[1;37m  ^^^^^^^^^^^^^^^^^^^^\033[0m")
+        print("^"*(63-x)+"\033[1;37m^^^^^^^^^^^^^^^^^^^^^^^^\033[0m")
+
+def difficulty():
+
+
+
+    print("Schwierigkeit wählen:")
+    print("- Diese beinflusst die Anzahl der verfügbaren Züge:\n ")
+
+    print(farbig("1. Einfach", 32) + " 15 Züge\n")
+    print(farbig("2. Mittel ", 32) + " 10 Züge\n")
+    print(farbig("3. Schwer ", 32) + "  5 Züge\n")
+    print(farbig("4. Exit   ", 31) + " Zurück in das Menü\n")
+
+    difficulty = 0
+
+    while difficulty != 1 and difficulty != 2 and difficulty != 3 and difficulty != 4:
+
+        try:
+            difficulty = int(input("Zahl eingeben (1-4):\n"))
+        except:
+            difficulty = 0
+        
+        if difficulty != 1 and difficulty != 2 and difficulty != 3 and difficulty != 4:
+
+            print("Ungültige Eingabe!")
+            
+    if difficulty in (1, 2, 3):
+        clear_terminal()
+        big_text("hangman")
+        print("Schwierigkeit", difficulty, "wurde ausgewählt!\n")
+        input("Enter drücken um fortzufahren:\n")
+        return difficulty
+    
+    if difficulty == 4:
+        clear_terminal()
+        game_pick_mode_hangman()
 
 def game_pick_mode_hangman():
 
+    clear_terminal()
 
     big_text("hangman")
 
     print("Bitte Spielmodus auswählen:\n ")
-    print(farbig("1. 1 v anyone", 32) + " mind. 1 Spieler\n---Einfach gegeneinander spielen!\n")
+    print(farbig("1. 1 v many", 32) + " mind. 1 Spieler\n---Einfach gegeneinander spielen!\n")
     print(farbig("2. Computer  ", 32) + " 1 Spieler\n---Gegen den Computer Spielen            (noch nicht verfügbar)\n")
     print(farbig("3. Exit      ", 31) + " \n---Spiel schließen\n")
 
@@ -55,14 +254,40 @@ def game_pick_mode_hangman():
             
     if mode == 1:
         clear_terminal()
-        game_hangman_1_v_anyone()
+        game_hangman_1_v_many()
     
     if mode == 3:
         clear_terminal()
         return
     
-def game_hangman_1_v_anyone():
-    print(1)
+def game_hangman_1_v_many():
+
+    big_text("hangman")
+    diffi = difficulty()
+    clear_terminal()
+
+    if diffi==1:
+
+        turns_multi = 0
+
+    elif diffi==2:
+
+        turns_multi = 5
+    else:
+
+        turns_multi = 10
+
+    while turns_multi <=15:
+        print("Zug Nr.", turns_multi)
+        print(hangman_pictures[turns_multi])
+        turns_multi =  turns_multi+1
+
+    
+
+    print("Leider verloren)")
+    input("Enter drücken um fortzufahren:\n")
+    game_pick_mode_hangman()
+        
 
 
 
@@ -75,4 +300,6 @@ def game_hangman_1_v_anyone():
 
 
 
-big_text("hangman")
+
+if __name__ == "__main__":
+    game_pick_mode_hangman()
