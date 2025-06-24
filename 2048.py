0c408660e717043948dbb162da5b9281c3aa3bdc
boris <boris@gratchev.com>
2025-06-23 20:26:04 -0700

    adding tiles

--- 2048.py
+++ 2048.py
@@ -1,3 +1,5 @@
+import random
+
 field_size = 4
 board = [[' ' for _ in range(field_size)] for _ in range(field_size)]
 
@@ -8,4 +10,22 @@ def print_board():
         print()
 
 
+print_board()
+
+def add_random_tile():
+    new_tile = 2
+    free_fields = []
+
+    # determine free fields
+    for i, row in enumerate(board):
+        for j, field in enumerate(row):
+            if field == ' ':
+                free_fields.append((i, j))
+
+    # pick a free field
+    picked_field = random.choice(free_fields)
+    board[picked_field[0]][picked_field[1]] = new_tile
+
+print()
+add_random_tile()
 print_board()
